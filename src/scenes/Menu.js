2efb796a95065ef6ca058e84d44d07701519f7c8
Yuwei-Mao <77122567+Yuwei-Mao@users.noreply.github.com>
2021-06-05 00:07:51 -0700

    Update Menu.js

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -11,10 +11,10 @@ class Menu extends Phaser.Scene{
     
     
     create() {
-        this.add.bitmapText(2*textSpacer, 3*textSpacer, 'gem', 'Press Left Arrow to move left.', 18).setOrigin(0,0).setTint(0x32a860);
-        this.add.bitmapText(2*textSpacer, 5*textSpacer, 'gem', 'Press Right Arrow to move right.', 18).setOrigin(0,0).setTint(0x32a860);
-        this.add.bitmapText(2*textSpacer, 7*textSpacer, 'gem', 'Press Shift Key to dash forward.', 18).setOrigin(0,0).setTint(0x32a860);
-        this.add.bitmapText(2*textSpacer, 9*textSpacer, 'gem', 'Press Space Bar to attack.', 18).setOrigin(0,0).setTint(0x32a860);
+        this.add.bitmapText(2*textSpacer, 3*textSpacer, 'gem', 'Press A to move left.', 18).setOrigin(0,0).setTint(0x32a860);
+        this.add.bitmapText(2*textSpacer, 5*textSpacer, 'gem', 'Press D to move right.', 18).setOrigin(0,0).setTint(0x32a860);
+        this.add.bitmapText(2*textSpacer, 7*textSpacer, 'gem', '', 18).setOrigin(0,0).setTint(0x32a860);
+        this.add.bitmapText(2*textSpacer, 9*textSpacer, 'gem', 'Press Mouse Left to attack.', 18).setOrigin(0,0).setTint(0x32a860);
         this.add.bitmapText(15*textSpacer, 13*textSpacer,'gem','Press Right Arrow to start the game.',10).setOrigin(0.5).setTint(0xff0000);
         this.add.bitmapText(4*textSpacer, 13*textSpacer,'gem','Press Left Arrow to go back.',10).setOrigin(0.5).setTint(0xff0000);
         cursors = this.input.keyboard.createCursorKeys();  
